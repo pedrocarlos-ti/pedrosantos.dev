4ac2f795a42d0f765e3eb70956e135f650a256c4
Pedro Santos <pedrocarlos.ti@gmail.com>
2026-01-14 09:49:43 +0000

    Fix overlapping navbars by using route groups for separate layouts

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import { Header } from "@/components/layout/header";
-import { Footer } from "@/components/layout/footer";
 import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
@@ -40,9 +38,7 @@ export default function RootLayout({
       >
         <ThemeProvider>
           <div className="flex min-h-screen flex-col">
-            <Header />
-            <main className="flex-1">{children}</main>
-            <Footer />
+            {children}
           </div>
           <Toaster />
           <Analytics />
